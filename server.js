e03471693e2d6c660d762004cb8f139de0d14db1
Djennad Rany <128905298+DjennadRany@users.noreply.github.com>
2024-07-18 13:53:00 +0200

    patch-5-boutontest
    patch-5-boutontest

--- server.js
+++ server.js
@@ -1,55 +1,61 @@
 "use strict";
 
+const pkg = require("./package.json");
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
+
 const errorHandler = require("errorhandler");
-const pkg = require("./package.json");
+const readdirSync = require("fs").readdirSync;
 
-const app = express();
-const PORT = 7770;
 
-// Configuration de base
-app.set("case sensitive routing", true);
-app.set("port", PORT);
-app.set("views", path.join(__dirname, "/src"));
-app.set("view engine", "pug");
+  const app = express();
 
-// Middleware pour CORS et headers
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+  app.set("case sensitive routing", true);
+  app.set("port", 7770);
+  app.set("views", __dirname + "/src");
+  app.set("view engine", "pug");
+
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.use(express.static(path.join(__dirname, "./temp")));
+console.log(path.join(__dirname, "./temp"))
+  // ---------
+  // PUG data
+  // ---------
+  app.locals.pretty = true;
+  app.locals.modules = pkg.subDependencies;
+  app.locals.cache = false;
+  app.locals.compileDebug = true;
+  app.use(
+    errorHandler({
+      dumpExceptions: true,
+      showStack: true,
+    })
   );
-  next();
-});
 
-// Serve static files from 'temp' directory
-app.use(express.static(path.join(__dirname, "temp")));
+  // --------------
+  // Routes | Pages
+  // --------------
+  const files = readdirSync(path.join(__dirname, "./src/")).filter((file) =>
+    file.endsWith(".pug")
+  );
 
-// PUG local variables
-app.locals = {
-  pretty: true,
-  modules: pkg.subDependencies,
-  cache: false,
-  compileDebug: true
-};
+  app.get("/", (req, res, next) => res.render("index"));
 
-// Error handling middleware
-app.use(errorHandler({ dumpExceptions: true, showStack: true }));
+  files.forEach((file) => {
+    const route = "" + file.split(".").slice(0, -1).join(".");
+    app.get(`/${route}.html`, (req, res, next) => {
+      res.render(route, { serverMode: true });
+    });
+  });
 
-// Dynamic route generation for Pug files
-const pugFiles = fs.readdirSync(path.join(__dirname, "src")).filter(file => file.endsWith(".pug"));
-pugFiles.forEach(file => {
-  const route = file.split(".").slice(0, -1).join(".");
-  app.get(`/${route}.html`, (req, res) => res.render(route, { serverMode: true }));
-});
-
-// Home route
-app.get("/", (req, res) => res.render("index"));
-
-// Start server
-app.listen(app.get("port"), () => {
-  console.log(`Express server listening on port ${app.get("port")}`);
-});
+  app.listen(app.get("port"), () =>
+    console.log("Express server listening on port " + app.get("port"))
+  );
